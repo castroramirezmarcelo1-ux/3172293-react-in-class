8e06d6fdfecf46e4f95b050c03823ffcb1a00dc8
Marcelo <castroramirezmarcelo1@gmail.com>
2026-05-25 07:56:21 -0500

    feat (components): Se creo el componente input y se implemento en el authlayout, tambien se creo el archivo index para encapsulamiento

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,10 +1,6 @@
-export default function App() {
-  return (
-    <div className="min-h-screen bg-green-800 flex items-center justify-center">
-      <h1 className="text-white text-4xl font-bold">
-        Con Marcelo programar Tailwind V4 funciona full...
-      </h1>
-    </div>
-  );
-} 
+import { RouterProvider } from "react-router-dom";
+import router from "@/app/router";
 
+export default function App() {
+  return <RouterProvider router={router} />;
+}
